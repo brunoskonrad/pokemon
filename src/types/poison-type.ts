61a7b929a0f9085a95d0b589202d0162d1f2f7da
Bruno Konrad <bruno.skon@gmail.com>
2019-12-18 17:20:47 +0100

    Create Poison type

--- src/types/poison-type.ts
+++ src/types/poison-type.ts
@@ -1,7 +1,41 @@
 import { Type } from "./type";
+import Effectiveness from "../effectiveness";
 
 export default class PoisonType extends Type {
   constructor() {
     super("poison");
+  }
+
+  attackEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "grass":
+      case "fairy":
+        return Effectiveness.for(2);
+      case "poison":
+      case "ground":
+      case "rock":
+      case "ghost":
+        return Effectiveness.for(0.5);
+      case "steel":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
+  }
+
+  defenseEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "ground":
+      case "psychic":
+        return Effectiveness.for(2);
+      case "fighting":
+      case "poison":
+      case "bug":
+      case "grass":
+      case "fairy":
+        return Effectiveness.for(0.5);
+      default:
+        return Effectiveness.for(1);
+    }
   }
 }
