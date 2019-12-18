3e57ca0d926dd8e88b1d9ffa4065d05ccd849589
Bruno Konrad <bruno.skon@gmail.com>
2019-12-18 22:08:04 +0100

    Create Dragon type

--- src/types/dragon-type.ts
+++ src/types/dragon-type.ts
@@ -1,7 +1,37 @@
 import { Type } from "./type";
+import Effectiveness from "../effectiveness";
 
 export default class DragonType extends Type {
   constructor() {
     super("dragon");
+  }
+
+  attackEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "dragon":
+        return Effectiveness.for(2);
+      case "steel":
+        return Effectiveness.for(0.5);
+      case "fairy":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
+  }
+
+  defenseEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "ice":
+      case "dragon":
+      case "fairy":
+        return Effectiveness.for(2);
+      case "fire":
+      case "water":
+      case "grass":
+      case "electric":
+        return Effectiveness.for(0.5);
+      default:
+        return Effectiveness.for(1);
+    }
   }
 }
