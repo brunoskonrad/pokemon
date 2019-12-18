2e421349f2898681a76c54b0ce898ce4fe52892e
Bruno Konrad <bruno.skon@gmail.com>
2019-12-18 22:48:04 +0100

    Create Dark type

--- src/types/dark-type.ts
+++ src/types/dark-type.ts
@@ -1,7 +1,38 @@
 import { Type } from "./type";
+import Effectiveness from "../effectiveness";
 
 export default class DarkType extends Type {
   constructor() {
     super("dark");
+  }
+
+  attackEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "ghost":
+      case "psychic":
+        return Effectiveness.for(2);
+      case "fighting":
+      case "dark":
+      case "fairy":
+        return Effectiveness.for(0.5);
+      default:
+        return Effectiveness.for(1);
+    }
+  }
+
+  defenseEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "fighting":
+      case "bug":
+      case "fairy":
+        return Effectiveness.for(2);
+      case "ghost":
+      case "dark":
+        return Effectiveness.for(0.5);
+      case "psychic":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
   }
 }
