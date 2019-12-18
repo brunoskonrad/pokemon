7c20a6809ca8d7a487363631e4ee52c9d90c701a
Bruno Konrad <bruno.skon@gmail.com>
2019-12-18 16:43:22 +0100

    Create Normal Type

--- src/types/normal-type.ts
+++ src/types/normal-type.ts
@@ -1,7 +1,31 @@
 import { Type } from "./type";
+import Effectiveness from "../effectiveness";
 
 export default class NormalType extends Type {
   constructor() {
     super("normal");
+  }
+
+  attackEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "steel":
+      case "rock":
+        return Effectiveness.for(0.5);
+      case "ghost":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
+  }
+
+  defenseEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "fighting":
+        return Effectiveness.for(2);
+      case "ghost":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
   }
 }
