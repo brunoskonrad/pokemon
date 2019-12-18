fcc40202e2b3e2ca01c9b61af847610e6820b35b
Bruno Konrad <bruno.skon@gmail.com>
2019-12-18 21:51:33 +0100

    Create Electric type

--- src/types/electric-type.ts
+++ src/types/electric-type.ts
@@ -1,7 +1,37 @@
 import { Type } from "./type";
+import Effectiveness from "../effectiveness";
 
 export default class ElectricType extends Type {
   constructor() {
     super("electric");
+  }
+
+  attackEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "flying":
+      case "water":
+        return Effectiveness.for(2);
+      case "grass":
+      case "electric":
+      case "dragon":
+        return Effectiveness.for(0.5);
+      case "ground":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
+  }
+
+  defenseEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "ground":
+        return Effectiveness.for(2);
+      case "flying":
+      case "steel":
+      case "electric":
+        return Effectiveness.for(0.5);
+      default:
+        return Effectiveness.for(1);
+    }
   }
 }
