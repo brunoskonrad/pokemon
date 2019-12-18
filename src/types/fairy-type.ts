1fee29a6435ed94342fe7ad03db920c04f07693f
Bruno Konrad <bruno.skon@gmail.com>
2019-12-18 22:55:44 +0100

    Create Fairy type

--- src/types/fairy-type.ts
+++ src/types/fairy-type.ts
@@ -1,7 +1,39 @@
 import { Type } from "./type";
+import Effectiveness from "../effectiveness";
 
 export default class FairyType extends Type {
   constructor() {
     super("fairy");
+  }
+
+  attackEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "fighting":
+      case "dragon":
+      case "dark":
+        return Effectiveness.for(2);
+      case "poison":
+      case "steel":
+      case "fire":
+        return Effectiveness.for(0.5);
+      default:
+        return Effectiveness.for(1);
+    }
+  }
+
+  defenseEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "poison":
+      case "steel":
+        return Effectiveness.for(2);
+      case "fighting":
+      case "bug":
+      case "dark":
+        return Effectiveness.for(0.5);
+      case "dragon":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
   }
 }
