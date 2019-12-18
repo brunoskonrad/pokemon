8540f640d9391158ed47b57d45035a8dbb3b43d7
Bruno Konrad <bruno.skon@gmail.com>
2019-12-18 18:59:18 +0100

    Create Steel type

--- src/types/steel-type.ts
+++ src/types/steel-type.ts
@@ -1,7 +1,48 @@
 import { Type } from "./type";
+import Effectiveness from "../effectiveness";
 
 export default class SteelType extends Type {
   constructor() {
     super("steel");
+  }
+
+  attackEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "rock":
+      case "ice":
+      case "fairy":
+        return Effectiveness.for(2);
+      case "steel":
+      case "fire":
+      case "water":
+      case "electric":
+        return Effectiveness.for(0.5);
+      default:
+        return Effectiveness.for(1);
+    }
+  }
+
+  defenseEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "fighting":
+      case "ground":
+      case "fire":
+        return Effectiveness.for(2);
+      case "normal":
+      case "flying":
+      case "rock":
+      case "bug":
+      case "steel":
+      case "grass":
+      case "psychic":
+      case "ice":
+      case "dragon":
+      case "fairy":
+        return Effectiveness.for(0.5);
+      case "poison":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
   }
 }
