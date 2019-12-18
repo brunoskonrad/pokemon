8f8f213594863ca6a36f1e60edc0477724e58cdc
Bruno Konrad <bruno.skon@gmail.com>
2019-12-18 21:59:04 +0100

    Create Psychic type

---
+++ src/types/__tests__/psychic-type.test.ts
@@ -0,0 +1,66 @@
+import NormalType from "../normal-type";
+import FightingType from "../fighting-type";
+import FlyingType from "../flying-type";
+import PoisonType from "../poison-type";
+import GroundType from "../ground-type";
+import RockType from "../rock-type";
+import BugType from "../bug-type";
+import GhostType from "../ghost-type";
+import SteelType from "../steel-type";
+import FireType from "../fire-type";
+import WaterType from "../water-type";
+import GrassType from "../grass-type";
+import ElectricType from "../electric-type";
+import PsychicType from "../psychic-type";
+import IceType from "../ice-type";
+import DragonType from "../dragon-type";
+import FairyType from "../fairy-type";
+import DarkType from "../dark-type";
+
+import { setupTestFor } from "../__jest__/effectiveness-test-helpers";
+
+describe("PsychicType", () => {
+  const subject = setupTestFor(new PsychicType());
+
+  describe(".attackEffectivenessOn", () => {
+    subject.testAttackEffectivenessOn(new NormalType(), 1);
+    subject.testAttackEffectivenessOn(new FightingType(), 2);
+    subject.testAttackEffectivenessOn(new FlyingType(), 1);
+    subject.testAttackEffectivenessOn(new PoisonType(), 2);
+    subject.testAttackEffectivenessOn(new GroundType(), 1);
+    subject.testAttackEffectivenessOn(new RockType(), 1);
+    subject.testAttackEffectivenessOn(new BugType(), 1);
+    subject.testAttackEffectivenessOn(new GhostType(), 1);
+    subject.testAttackEffectivenessOn(new SteelType(), 0.5);
+    subject.testAttackEffectivenessOn(new FireType(), 1);
+    subject.testAttackEffectivenessOn(new WaterType(), 1);
+    subject.testAttackEffectivenessOn(new GrassType(), 1);
+    subject.testAttackEffectivenessOn(new ElectricType(), 1);
+    subject.testAttackEffectivenessOn(new PsychicType(), 0.5);
+    subject.testAttackEffectivenessOn(new IceType(), 1);
+    subject.testAttackEffectivenessOn(new DragonType(), 1);
+    subject.testAttackEffectivenessOn(new DarkType(), 0);
+    subject.testAttackEffectivenessOn(new FairyType(), 1);
+  });
+
+  describe(".defenseEffectivenessOn", () => {
+    subject.testDefendEffectivenessOn(new NormalType(), 1);
+    subject.testDefendEffectivenessOn(new FightingType(), 0.5);
+    subject.testDefendEffectivenessOn(new FlyingType(), 1);
+    subject.testDefendEffectivenessOn(new PoisonType(), 1);
+    subject.testDefendEffectivenessOn(new GroundType(), 1);
+    subject.testDefendEffectivenessOn(new RockType(), 1);
+    subject.testDefendEffectivenessOn(new BugType(), 2);
+    subject.testDefendEffectivenessOn(new GhostType(), 2);
+    subject.testDefendEffectivenessOn(new SteelType(), 1);
+    subject.testDefendEffectivenessOn(new FireType(), 1);
+    subject.testDefendEffectivenessOn(new WaterType(), 1);
+    subject.testDefendEffectivenessOn(new GrassType(), 1);
+    subject.testDefendEffectivenessOn(new ElectricType(), 1);
+    subject.testDefendEffectivenessOn(new PsychicType(), 0.5);
+    subject.testDefendEffectivenessOn(new IceType(), 1);
+    subject.testDefendEffectivenessOn(new DragonType(), 1);
+    subject.testDefendEffectivenessOn(new DarkType(), 2);
+    subject.testDefendEffectivenessOn(new FairyType(), 1);
+  });
+});
--- src/types/psychic-type.ts
+++ src/types/psychic-type.ts
@@ -1,7 +1,37 @@
 import { Type } from "./type";
+import Effectiveness from "../effectiveness";
 
 export default class PsychicType extends Type {
   constructor() {
     super("psychic");
+  }
+
+  attackEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "fighting":
+      case "poison":
+        return Effectiveness.for(2);
+      case "steel":
+      case "psychic":
+        return Effectiveness.for(0.5);
+      case "dark":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
+  }
+
+  defenseEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "bug":
+      case "ghost":
+      case "dark":
+        return Effectiveness.for(2);
+      case "fighting":
+      case "psychic":
+        return Effectiveness.for(0.5);
+      default:
+        return Effectiveness.for(1);
+    }
   }
 }
