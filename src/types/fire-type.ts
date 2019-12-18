aaca664741972f8db9c1c82f3f3b88882d7f77b5
Bruno Konrad <bruno.skon@gmail.com>
2019-12-18 19:13:36 +0100

    Create Fire type

--- src/types/fire-type.ts
+++ src/types/fire-type.ts
@@ -1,7 +1,43 @@
 import { Type } from "./type";
+import Effectiveness from "../effectiveness";
 
 export default class FireType extends Type {
   constructor() {
     super("fire");
+  }
+
+  attackEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "bug":
+      case "steel":
+      case "grass":
+      case "ice":
+        return Effectiveness.for(2);
+      case "rock":
+      case "fire":
+      case "water":
+      case "dragon":
+        return Effectiveness.for(0.5);
+      default:
+        return Effectiveness.for(1);
+    }
+  }
+
+  defenseEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "ground":
+      case "rock":
+      case "water":
+        return Effectiveness.for(2);
+      case "bug":
+      case "steel":
+      case "fire":
+      case "grass":
+      case "ice":
+      case "fairy":
+        return Effectiveness.for(0.5);
+      default:
+        return Effectiveness.for(1);
+    }
   }
 }
