2212c2ee766484529f86b94a118e9299f5b3ca44
Bruno Konrad <bruno.skon@gmail.com>
2019-12-18 18:47:23 +0100

    Create Ghost type

--- src/types/ghost-type.ts
+++ src/types/ghost-type.ts
@@ -1,7 +1,38 @@
 import { Type } from "./type";
+import Effectiveness from "../effectiveness";
 
 export default class GhostType extends Type {
   constructor() {
     super("ghost");
+  }
+
+  attackEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "ghost":
+      case "psychic":
+        return Effectiveness.for(2);
+      case "dark":
+        return Effectiveness.for(0.5);
+      case "normal":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
+  }
+
+  defenseEffectivenessOn(type: Type) {
+    switch (type.name) {
+      case "ghost":
+      case "dark":
+        return Effectiveness.for(2);
+      case "poison":
+      case "bug":
+        return Effectiveness.for(0.5);
+      case "normal":
+      case "fighting":
+        return Effectiveness.for(0);
+      default:
+        return Effectiveness.for(1);
+    }
   }
 }
